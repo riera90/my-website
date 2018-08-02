a01e44205e6597c22ae44e341606358bff0693ae
riera90 <diegorodriguezriera@gmail.com>
2018-08-02 14:20:06 +0200

    added terminal basic backend and database

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,18 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Terminal;
+use App\Form\TerminalType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/homepage", name="homepage")
      */
-    public function index()
+    public function home(Request $request, SessionInterface $session)
     {
+        $phpsessid=$session->getId();
+        $entityManager=$this->getDoctrine()->getManager();
+        $terminals=$entityManager->getRepository('App:Terminal')->findTerminalByPhpsessid($phpsessid);
+
+        if (count($terminals)==0)
+        {
+            $terminal = new Terminal();
+            $terminal->setPhpsessid($phpsessid);
+        }elseif (count($terminals)==1){
+            $terminal=$terminals[0];
+        }else{
+            //todo: remove all except one terminal
+            $stupidVar=0;
+        }
+
+        dump($phpsessid);
+
+
+        dump($terminals);
+
+        $form = $this->createForm(TerminalType::class, $terminal);
+        //dump($terminal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $terminal->addLine($terminal->getInput());
+            $entityManager->persist($terminal);
+            $entityManager->flush();
+
+            return $this->render('homepage/index.html.twig', [
+                'controller_name' => 'HomepageController',
+                'lines' => $terminal->getLines(),
+                'form'=> $form->createView(),
+            ]);
+        }
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'lines' => $terminal->getLines(),
+            'form'=> $form->createView(),
         ]);
     }
+
+
 }
